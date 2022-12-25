0fcd9db312715f19b62a98098e068c8bf613ce1a
Dimitry Perchatkin <perchatkindimitry@gmail.com>
2022-12-26 00:35:45 +0300

    Fix server bugs

--- main.py
+++ main.py
@@ -5,23 +5,23 @@ app = Flask(__name__, template_folder="", static_folder="")
 
 @app.route("/")
 def index():
-    return "Тут будет главная страница, если успеем"
+    return render_template("index.html.", title="Главная")
 
-@app.route("/hourse_module")
+@app.route("/horse_module")
 def hourse_module():
-    return render_template("index.html", folder="hourse")
+    return render_template("module.html", folder="horse", width="850", height="650", title="horse")
 
 @app.route("/huffman_module")
 def huffman_module():
-    return render_template("index.html", folder="huffman")
+    return render_template("module.html", folder="huffman", width="900", height="650", title="huffman")
 
 @app.route("/kruskal_module")
 def kruskal_module():
-    return render_template("index.html", folder="kruskal")
+    return render_template("module.html", folder="kruskal", width="910", height="620", title="kruskal")
 
-# @app.route("/")
-# def index():
-#     return render_template("index.html")
+# @app.route("/dijkstra_module")
+# def dijkstra_module():
+#     return render_template("index.html", folder="dijkstra", width="850", height="750", title="dijkstra")
 
 
 if __name__ == "__main__":
